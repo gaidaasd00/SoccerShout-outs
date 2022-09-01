30618b83db37c5a47885282970135f88da8d3a73
gaidaasd00 <swiftgaida@list.ru>
2022-09-01 19:02:27 +0300

    1. setup project 2. add uiTableView 3. add cell 4. setting constraint 5. add resources and assets

---
+++ SoccerShout-outs/ViewControllers/ChantsViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ChantsViewController.swift
+//  SoccerShout-outs
+//
+//  Created by Алексей Гайдуков on 01.09.2022.
+//
+
+import UIKit
+
+class ChantsViewController: UIViewController {
+
+    //MARK: - UI
+    
+    private let tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tableView
+    }()
+    
+    
+    // MARK: - Lyfecycle
+    //make adjustments more efficiently UI
+    override func loadView() {
+        super.loadView()
+        setup()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemPink
+    }
+}
+    // MARK: - Extension
+extension ChantsViewController {
+    func setup() {
+        //allows you to return the data we want to see on the screen
+        tableView.dataSource = self
+        
+        self.view.addSubview(tableView)
+        
+        //setupConstreins
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+//MARK: - UITableViewDataSource
+extension ChantsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        switch indexPath.row {
+        case 0:
+            cell.backgroundColor = .systemMint
+        case 1:
+            cell.backgroundColor = .systemCyan
+        default:
+            cell.backgroundColor = .systemGray
+        }
+        return cell
+    }
+    
+    
+}
