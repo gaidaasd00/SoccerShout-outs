ec5361f98b71ba7387b4e521df8a3a50df8b9e2b
gaidaasd00 <swiftgaida@list.ru>
2022-09-01 17:47:15 +0300

    Initial Commit

---
+++ SoccerShout-outs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoccerShout-outs
+//
+//  Created by Алексей Гайдуков on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
